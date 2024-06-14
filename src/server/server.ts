49b4783bcef845888593c399ab8904e15220f75d
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-06-14 16:30:14 -0400

    wokring build script

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,28 @@
-import { createServer } from "http";
 import express from "express";
+import { ExpressAuth } from "@auth/express"
+import { TypeORMAdapter } from "@auth/typeorm-adapter"
 
 export const server = async () => {
   const app = express();
 
+  app.set("trust proxy", true)
+  app.use(
+    "/auth/*",
+    ExpressAuth({
+      providers: [],
+      adapter: TypeORMAdapter(process.env.AUTH_TYPEORM_CONNECTION || "sqlite://:memory:"),
+    })
+  )
+
   app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: __dirname });
+    res.sendFile("index.html", { root: "./public" });
   });
 
-  const server = createServer(app);
+  app.use(express.static("./public"));
 
-  server.listen(4000, () => {
-    console.log(`Server running in 4000`);
-  });
+  const port = process.env.PORT || 4000
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 };
